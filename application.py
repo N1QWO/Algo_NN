36224da80551d7c9a926f47578a87b28c1648dc2
N1QWO <76246392+N1QWO@users.noreply.github.com>
2024-10-07 19:54:28 +0300

    application.py

---
+++ application.py
@@ -0,0 +1,21 @@
+from main import model
+import numpy as np
+
+sample_ = 1000
+    # Количество примеров
+in_ = 10  # Размерность каждого примера
+out_ = 1
+He = [32,128,32]
+h = [in_,*He,out_]
+# Генерация данных
+X = np.random.rand(sample_, in_)
+
+    # Преобразование данных и расчет меток
+Y = np.sin(np.sum(X**2,axis=1))  # Суммирование по каждой строке
+Y = Y.reshape(Y.shape[0],1)
+
+sem = 0.8
+X_train,X_test,Y_train,Y_test = X[:int(sample_*sem)],X[int(sample_*sem):sample_],Y[:int(sample_*sem)],Y[int(sample_*sem):sample_]
+
+NN = model(h,lr=1e-3)
+NN.fit(X_train,X_test,Y_train,Y_test,epoch=1000,batch_size=128)
